f23600cd6fdeddd44e71ab532c207ee4dec4ce52
Andrea Hostetter <alhostetter@gmail.com>
2018-11-07 12:44:52 -0600

    Add dotenv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Game.destroy_all
+
+puts 'Creating games...'
+games_attributes = [
+  {
+    title:         'Dirreciones',
+
+  },
+  {
+    title:         'Pizza East',
+    slug:      '56A Shoreditch High St, London E1 6PQ',
+  },
+  {
+    title:         'Dishoom',
+    slug:          '7 Boundary St, London E2 7JE',
+  },
+  {
+    title:         'Pizza East',
+    slug:      '56A Shoreditch High St, London E1 6PQ',
+  },
+  {
+    title:         'Dishoom',
+    slug:          '7 Boundary St, London E2 7JE',
+  },
+  {
+    title:         'Pizza East',
+    slug:      '56A Shoreditch High St, London E1 6PQ',
+  },
+  {
+    title:         'Dishoom',
+    slug:          '7 Boundary St, London E2 7JE',
+  },
+  {
+    title:         'Pizza East',
+    slug:      '56A Shoreditch High St, London E1 6PQ',
+  }
+]
+Game.create!(games_attributes)
+puts 'Finished!'
