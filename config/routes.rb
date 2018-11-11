f581ba7e2a215b92a7718b5e696aa0ac3b212b2c
Andrea Bazan <andrea.bazan.polanco@gmail.com>
2018-11-10 20:46:58 -0600

    New project from template

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'levels/play'
   devise_for :users
   root to: 'pages#home'
-  # get 'user/dashboard'
 
   resources :games, only: %i[show] do
     resources :levels, only: %i[] do
@@ -13,5 +11,4 @@ Rails.application.routes.draw do
   end
 
   get 'users/dashboard' => 'users#dashboard', as: :user_root # creates user_root_path
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/levels_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class LevelsControllerTest < ActionDispatch::IntegrationTest
-  test "should get play" do
-    get levels_play_url
-    assert_response :success
-  end
-
-end
