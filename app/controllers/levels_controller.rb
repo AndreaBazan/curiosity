69d743ccce59fe1e89549a9a3e2ed74edc02fc2c
Ana Tellez <ana.tellez.r@gmail.com>
2018-11-13 16:22:58 -0600

    board creator created

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -1,4 +1,13 @@
 class LevelsController < ApplicationController
   def play
+    @level = Level.find(params[:id])
+    # @board = @level.board
+    @board = [
+      %i[robot empty empty hole],
+      %i[hole hole empty hole],
+      %i[empty wall empty wall],
+      %i[empty wall empty wall],
+      %i[empty empty empty hole]
+    ]
   end
 end
---
+++ app/javascript/packs/game.js
@@ -0,0 +1,152 @@
+const robotPiece = document.querySelector('.robot-piece');
+const btns = document.querySelectorAll(".race-btns");
+
+// check if a tile have an empty neighbour
+function canMove(direction) {
+  // Propiedades del tile
+  let robot = document.querySelector('.robot');
+
+  // Direction
+  switch (direction) {
+    case 'left' :
+      // LEFT
+      if(robot.cellIndex == 0) return false;
+      let left = robot.previousElementSibling.className.includes('obstaculo');
+      return !left;
+      break;
+    case 'up' :
+      // UP
+      if (robot.parentElement.rowIndex == 0) return false;
+      let up = robot.parentElement.previousElementSibling.children[robot.cellIndex].className.includes('obstaculo');
+      return !up;
+      break;
+    case 'right':
+      // RIGHT
+      if(!robot.nextElementSibling) return false;
+      let right = robot.nextElementSibling.className.includes('obstaculo');
+      return !right;
+      break;
+
+    case 'down' :
+      // DOWN
+      if (!robot.parentElement.nextElementSibling ) return false;
+      let down = robot.parentElement.nextElementSibling.children[robot.cellIndex].className.includes('obstaculo');
+      return !down;
+      break;
+      default:
+      console.log('Something went wrong');
+  }
+}
+
+// Move the tile
+function moveTile(direction) {
+  // Propiedades del tile
+  let robot = document.querySelector('.robot');
+  let move;
+  let x;
+  let y;
+
+  // Direction
+  switch (direction) {
+    case 'left' :
+
+      move = robot.previousElementSibling;
+      x = move.offsetLeft;
+      y = move.offsetTop;
+      // Change absolute position of the robot to the
+      robot.classList.toggle("robot");
+      move.classList.add("robot");
+      // LEFT
+      // Add #robot to previousSibling && remove #robot to Actual cell
+      // Return boolean
+
+      break;
+
+    case 'up' :
+      move = robot.parentElement.previousElementSibling.children[robot.cellIndex];
+      x = move.offsetLeft;
+      y = move.offsetTop;
+      // Change absolute position of the robot to the
+      robot.classList.toggle("robot");
+      move.classList.add("robot")
+
+      // UP
+      // Get actual row by Parent Element && previousRow && cell by CellIndex
+      break;
+
+    case 'right' :
+      move = robot.nextElementSibling;
+      x = move.offsetLeft;
+      y = move.offsetTop;
+      // Change absolute position of the robot to the
+      robot.classList.toggle("robot");
+      move.classList.add("robot");
+
+      // RIGHT
+      // Add #robot to nextElementSibling && remove #robot to Actual cell
+      break;
+
+    case 'down' :
+      move = robot.parentElement.nextElementSibling.children[robot.cellIndex];
+      x = move.offsetLeft;
+      y = move.offsetTop;
+      // Change absolute position of the robot to the
+      robot.classList.toggle("robot");
+      move.classList.add("robot")
+      // DOWN
+      // Get actual row by Parent Element && nextRow && cell by CellIndex
+      break;
+    default:
+      console.log('Something went wrong');
+  }
+  setPiecePosition();
+}
+
+
+let playerMoves = [];
+
+function queueMove () {
+   const inputID = this.getAttribute('data-input');
+  const times = document.getElementById(inputID).value;
+  const direction = this.id;
+
+}
+
+//function printMoves() {};
+
+function moveToDirection() {
+  const inputID = this.getAttribute('data-input');
+  const times = document.getElementById(inputID).value;
+  const direction = this.id;
+
+  multipleMoves(direction, times);
+}
+
+btns.forEach(btn => {
+  btn.onclick = moveToDirection;
+});
+
+var multipleMoves = function(direction, times) {
+  return new Promise(function(resolve) {
+    for (let i = 0; i < times; i++){
+      if (canMove(direction)) {
+        moveTile(direction);
+      }
+    }
+    setTimeout( _ => {
+      resolve(window);
+    }, 1000);
+  });
+}
+
+window.multipleMoves = multipleMoves;
+
+function setPiecePosition() {
+  console.log('Set piece')
+  let robot = document.querySelector('.robot');
+  robotPiece.style.left = robot.offsetLeft + 'px'
+  robotPiece.style.top = robot.offsetTop + 'px'
+}
+
+// Initial Game State
+setPiecePosition();
