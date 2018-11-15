6d507634370c69cca160d469a7fa4fbcc81abbf8
Andrea Bazan <andrea.bazan.polanco@gmail.com>
2018-11-14 19:08:35 -0600

    added board to levels and store movements

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -1,13 +1,6 @@
 class LevelsController < ApplicationController
   def play
     @level = Level.find(params[:id])
-    # @board = @level.board
-    @board = [
-      %i[empty empty empty hole],
-      %i[hole hole empty hole],
-      %i[empty wall robot wall],
-      %i[empty wall empty wall],
-      %i[empty empty empty hole]
-    ]
+    @board = @level.board
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,3 @@
 class Game < ApplicationRecord
-  has_many :levels
+  has_many :levels, dependent: :destroy
 end
